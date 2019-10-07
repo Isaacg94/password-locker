fe592fa1a1b383d9469cbcdc9120d55f1677ca04
Isaacg94 <7248zack@gmail.com>
2019-10-07 08:18:40 +0300

    add initial structure of the page

---
+++ password_test.py
@@ -0,0 +1,4 @@
+import unittest
+from passwords import User, Credentials
+
+
---
+++ passwords.py
@@ -0,0 +1,33 @@
+class User:
+    """
+    Class that stores new instances of users.
+    """
+
+
+
+def __init__(self, first_name, last_name, password):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+
+class Credentials:
+    """
+    Class that stores new instances of users credentials.
+    """
+
+
+credentials_list = []
+
+
+def __init__(self, site, user_name, password):
+
+        self.site = site
+        self.user_name = user_name
+        self.password = password
+
+
+new_credential = Credential("instagram", "isaacg94", "12345678")
+
+print(new_credential.site,new_credential.user_name,new_credential.password)
